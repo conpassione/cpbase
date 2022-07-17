87c9188d0de536443c3e9c6a362f7c97c0b6d1cb
Renzo Bauen <info@conpassione.ch>
2022-07-17 14:30:11 +0200

    Renamed Ext-Key

--- ext_tables.php
+++ ext_tables.php
@@ -1,7 +1,7 @@
 <?php
 /*
  #---------------------------------------------------------------
- # ext_tables file for ext cp_base
+ # ext_tables file for ext cpbase
  #
  # Generated 01.06.2020 by www.conpassione.ch
  #---------------------------------------------------------------
@@ -11,8 +11,8 @@ if (!defined('TYPO3_MODE')) {
     die ('Access denied.');
 }
 
-$GLOBALS['TBE_STYLES']['skins']['cp_base']['name'] = 'cp_base';
+$GLOBALS['TBE_STYLES']['skins']['cpbase']['name'] = 'cpbase';
 
-$GLOBALS['TBE_STYLES']['skins']['cp_base']['stylesheetDirectories']['cp_base_additional'] = 'EXT:cp_base/Resources/Public/Backend/Css/Skin/';
+$GLOBALS['TBE_STYLES']['skins']['cpbase']['stylesheetDirectories']['cpbase_additional'] = 'EXT:cpbase/Resources/Public/Backend/Css/Skin/';
 
 ?>

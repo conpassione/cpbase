d80778eccfcfd1083b9f0104512d1696bd9108db
Renzo Bauen <info@conpassione.ch>
2022-09-02 19:44:22 +0200

    SQL statement

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,6 +2,6 @@
 # TYPO3 V11, GNU 2.0
 #
 CREATE TABLE tt_content (
-#	ce_width varchar(32) DEFAULT '' NOT NULL,
+	ce_width varchar(32) DEFAULT '' NOT NULL,
 	ce_width varchar(32) DEFAULT 'normal' NOT NULL
 );

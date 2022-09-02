e5878c7a4b25c7520b5c25c1cb523cc67cf3435f
Renzo Bauen <info@conpassione.ch>
2022-09-02 19:32:06 +0200

    PageConfig Added, gridelements eliminated

---
+++ Classes/PageConfiguration.php
@@ -0,0 +1,49 @@
+
+
+<?php
+
+declare(strict_types=1);
+
+namespace B13\SiteT3demo;
+
+/*
+
+ * This file is part of TYPO3 CMS-extension site_t3demo by b13.
+
+ *
+
+ * It is free software; you can redistribute it and/or modify it under
+
+ * the terms of the GNU General Public License, either version 2
+
+ * of the License, or any later version.
+
+ */
+
+/**
+
+ * Demo project has specific backend layouts based on the page type,
+
+ * which is mapped in here.
+
+ */
+
+class PageConfiguration
+
+{
+    public const DOKTYPE_CONTENTPAGE = 1;
+    public const DOKTYPE_STARTPAGE = 366510;
+
+    protected array $backendLayoutMapping = [
+        self::DOKTYPE_CONTENTPAGE => 'pagets__Contentpage',
+        self::DOKTYPE_STARTPAGE => 'pagets__Startpage',
+    ];
+
+    public function getBackendLayout(int $doktype): string
+    {
+        if (empty($this->backendLayoutMapping[$doktype])) {
+            throw new \Exception('No backend layout mapping for doktype ' . $doktype, 1553253111);
+        }
+        return $this->backendLayoutMapping[$doktype];
+    }
+}

3114133a0e54f390aeff87bd59874ef21b891aee
Renzo Bauen <info@conpassione.ch>
2022-09-17 17:48:11 +0200

    Sample Testfile created

---
+++ Tests/Unit/Hooks/DataHandlerFlushByTagHookTest.php
@@ -0,0 +1,15 @@
+ <?php
+ namespace Lolli\Enetcache\Tests\Unit\Hooks;
+
+ use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+ class DataHandlerFlushByTagHookTest extends UnitTestCase
+ {
+     /**
+      * @test
+      */
+     public function findReferencedDatabaseEntriesReturnsEmptyArrayForTcaWithoutRelations()
+     {
+         // some unit test code
+     }
+ }
